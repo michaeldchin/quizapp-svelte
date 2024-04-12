3261a388d05ea10f9998a70b48da6007cb03ab90
michaeldchin <mchin461@gmail.com>
2024-04-12 17:24:01 -0500

    trying to make host + websockets

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,34 @@
 <script>
   import logo from './assets/triviaapp-mc.png'
-  import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+  import Host from './lib/Host.svelte'
+  let gameState = ''
 </script>
 
 <main>
-  <div>
-    <img src={logo} class="logo" alt="Logo" />
-  </div>
+  
+  {#if gameState === ''}
+    <div>
+      <img src={logo} class="logo" alt="Logo" />
+    </div>
 
-  <div class="card">
-    <Counter />
+    <div class="card">
+    <button on:click={() => gameState = 'host'}>
+      Host a new game
+    </button>
   </div>
+  {:else if gameState === 'host'}
+    <Host />
+  {/if}
 </main>
 
 <style>
   .logo {
-    height: 50vh;
+    height: 25vh;
     padding: 1.5em;
     will-change: filter;
     transition: filter 300ms;
+    background-color: lightcyan;
   }
   .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
--- src/lib/Host.svelte
+++ src/lib/Host.svelte
@@ -1,3 +1,13 @@
 <script>
-    WebSocket()
+  const gameId = localStorage.getItem("gameId");
+  if (gameId == null) {
+
+  }
+  const ws = new WebSocket(`ws://localhost:8080/?player=host&gameId=${gameId}`);
+
+  // localStorage.setItem('gameId', 'aaab')
 </script>
+
+<main>
+  <p>stuuff</p>
+</main>
