5ec9f5ec8e0ec0498eb9f6c99d327f2008fa3718
Michael Chin <mchin461@gmail.com>
2024-04-15 17:14:30 -0500

    client host gets response from server

--- server.js
+++ server.js
@@ -2,16 +2,26 @@ import { WebSocketServer } from 'ws'
 import queryString from 'querystring'
 const wss = new WebSocketServer({ port: 8080 });
 
+class Roles {
+  static HOST = 'host'
+  static PLAYER = 'player'
+  static VIEWER = 'viewer'
+}
+
+
 wss.on('connection', (ws, req) => {
 
   logConnectionInfo(req)
+  // Host has started game or player/viewer joins
+  handleGameRequest(ws, req)
 
+  
 
   ws.on('error', console.error);
   ws.on('message', (message) => {
-    const controllerResponse = Controller.handle(JSON.parse(message))
-    console.log(controllerResponse)
-    ws.send(controllerResponse)
+    // const controllerResponse = Controller.handle(JSON.parse(message))
+    // console.log(controllerResponse)
+    // ws.send(controllerResponse)
   })
 
   ws.on('close', () => {
@@ -26,17 +36,18 @@ const logConnectionInfo = (req) => {
   console.log(`New connection ${ip} connections: ${wss.clients.size} with gameId: ${queryParams.gameId}`)
 }
 
-const handleGameRequest = (req) => {
+const handleGameRequest = (ws, req) => {
   const queryParams = queryString.parse(req.url.replace(/^.*\?/, ''))
-  if (queryParams.player === 'host') {
+  if (queryParams.player === Roles.HOST) {
     // host starting new game
-    Controller.newGame()
+    const newGameId = Controller.newGame()
+    ws.send(JSON.stringify({role: Roles.HOST, gameId: newGameId}))
     // or maybe host rejoining
   }
-  if (queryParams.player === 'player') {
+  if (queryParams.player === Roles.PLAYER) {
     
   }
-  if (queryParams.player === 'viewer') {
+  if (queryParams.player === Roles.VIEWER) {
     
   }
 }
@@ -45,25 +56,11 @@ class Controller {
 
   static games = new Map()
 
-  static handle(input) {
-    const command = input.command
-    this.newGame()
-    console.log('Controller.handle() input: ', command)
-    switch (command) {
-      case 'host':
-        return this.newGame()// should return status of 
-      case this.games.has(command):
-        console.log('GAME HAS INPUT WORKS')
-        return this.listPlayers(command)
-      default:
-        return 'DEFAULT'
-    }
-  }
-
   static newGame() {
-    const gameId = 'aaaa'
+    const gameId = 'aaaa' //todo: change to rng
     this.games.set(gameId, new Game(gameId))
-    return JSON.stringify({ command: 'host', gameId})
+    
+    return gameId
   }
 
   static listPlayers(input) {
@@ -71,10 +68,16 @@ class Controller {
   }
 }
 
+class GameStatus {
+  static ACTIVE = 'active' 
+  static INACTIVE = 'inactive'
+}
+
 class Game {
   constructor(gameId) {
     this.gameId = gameId
     this.players = []
+    this.status = GameStatus.ACTIVE
   }
   
   addPlayer(playerName) {
--- src/lib/Host.svelte
+++ src/lib/Host.svelte
@@ -5,9 +5,16 @@
   }
   const ws = new WebSocket(`ws://localhost:8080/?player=host&gameId=${gameId}`);
 
+  export let response = null
+  ws.onmessage = msg => {
+    console.log(msg)
+    response = msg.data
+    
+  }
   // localStorage.setItem('gameId', 'aaab')
 </script>
 
 <main>
   <p>stuuff</p>
+  <p>{response}</p>
 </main>
