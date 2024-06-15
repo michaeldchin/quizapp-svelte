d61229b47fe27da1133534a11f8bc647b7d07aa3
michaeldchin <mchin461@gmail.com>
2024-06-15 18:37:16 -0500

    color button

--- src/lib/Scorecard.svelte
+++ src/lib/Scorecard.svelte
@@ -1,5 +1,21 @@
 <script>
     export let player;
+    let minusColor;
+    let plusColor;
+
+    function adjustScore(amount) {
+        player.scoreDelta += amount;
+        if (player.scoreDelta < 0) {
+            minusColor = "rgba(255, 0, 0, 0.5)";
+            plusColor = "white";
+        } else if (player.scoreDelta > 0) {
+            plusColor = "rgba(0, 128, 0, 0.5)";
+            minusColor = "white";
+        } else {
+            plusColor = "white";
+            minusColor = "white";
+        }
+    }   
 </script>
 
 <main>
@@ -13,13 +29,17 @@
             </div>
         </div>
         <div class="scoringSection">
-            <h3>{player.scoreDelta}</h3>
-            <button class="scoreButton" on:click={() => player.scoreDelta--}>-1</button>
-            <button class="scoreButton" on:click={() => player.scoreDelta++}>+1</button>
+            <h3 class="scoreLabel">{player.scoreDelta}</h3> 
+            <button class="scoreButton" style="background-color: {minusColor};" on:click={() => adjustScore(-1)}>-1</button>
+            <button class="scoreButton" style="background-color: {plusColor};" on:click={() => adjustScore(1)}>+1</button>
             <h3>Total Score: {player.score}</h3>
         </div>
     </div>
 </main>
 
 <style>
+    .scoreLabel {
+        font-size: x-large;
+        font-weight: bold;
+    }
 </style>
