f4b9e1821ba25c7ec613863a150a72a5b1432335
michaeldchin <mchin461@gmail.com>
2024-06-17 00:09:44 -0500

    moved keepalive log for player

--- server.js
+++ server.js
@@ -110,13 +110,13 @@ wss.on('connection', (ws, req) => {
   // Player Events
   if (role === Roles.PLAYER) {
     ws.on('message', (message) => {
-      console.log(JSON.parse(message)) 
       const resp = JSON.parse(message)
       if (resp.event ==='keepAlive') {
         // in prod the idle wss connection is killed after 60 seconds, so we take keepalive pings
         // https://stackoverflow.com/questions/29579208/why-does-my-websocket-close-after-a-few-minutes
         return
       }
+      console.log(JSON.parse(message)) 
       if (resp.event === 'playerAnswer') {
         // player updated answer
         const game = Controller.getGame(gameId);
